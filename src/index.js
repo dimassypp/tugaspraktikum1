70e8f0c2766170432406a748260213229a85d288
dimassypp <inidype@gmail.com>
2022-03-16 08:58:36 +0700

    praktikum1

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import No1 from './pages/no1'
+import No2 from './pages/no2'
+import No3 from './pages/no3'
+import No4desimal from './pages/no4desimal'
+import No4biner from './pages/no4biner'
+import No4hexadesimal from './pages/no4hexadesimal'
+import No4oktal from './pages/no4oktal'
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <No1 />
   </React.StrictMode>,
   document.getElementById('root')
 );
